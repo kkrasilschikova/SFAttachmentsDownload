8e5ae577b1bc0dc8b808d2d92acb06ff1244a37b
kkrasilschikova <ksenia.krasilschikova@veeam.com>
2017-12-28 15:02:58 +0300

    update main with scopt

--- src/main/scala/SfAttachDownload/Main.scala
+++ src/main/scala/SfAttachDownload/Main.scala
@@ -6,103 +6,74 @@ import scala.util.{Failure, Success, Try}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    if (System.getProperty("os.name").matches(".*Windows.*")) {
-      //actions in case of Windows
-      args.length match {
-        case 1 =>
-          val caseNumber = args(0)
-          if (caseNumber.length != 8) {
-            println("Please enter valid case number.")
-            System.exit(1)
-          }
+    case class Config(caseNumber: String = "", targetFolder: String = "", userName: String = "", passwordToken: String = "",
+                      threads: Int = Runtime.getRuntime.availableProcessors() - 1, only: Int = 2, ftp: Boolean = true)
+
+    val parser = new scopt.OptionParser[Config]("salesforce-attachments-download") {
+      opt[String]('c', "caseNumber").required().valueName("<8_digit_value>")
+        .text("required").action((x, c) => c.copy(caseNumber = x))
+        .validate(x => if (x.matches("\\d{8}")) success else failure("Invalid case number"))
+      opt[String]('d', "destinationFolder").optional().valueName("<target_for_downloads>")
+        .text("optional, by default current_dir\\case_number").action((x, c) => c.copy(targetFolder = x))
+      opt[String]('u', "userName").optional().valueName("<SF_username>")
+        .text("required on Windows for the first run only").action((x, c) => c.copy(userName = x))
+      opt[String]('p', "passwordToken").optional().valueName("<SFpasswordToken (no spaces)>")
+        .text("required on Windows for the first run only").action((x, c) => c.copy(passwordToken = x))
+      opt[Int]('t', "threads").optional().valueName("<number_of_threads>")
+        .text("optional, by default equal to available CPUs-1").action((x, c) => c.copy(threads = x))
+        .validate(x => if (x != 0) success else failure("Not enough threads"))
+      opt[Int]('o', "only").optional().valueName("<0, 1 or nothing>")
+        .text("optional, if 1 then download only the latest attachment(s), if 0 - only the first, by default download all files")
+        .action((x, c) => c.copy(only = x))
+      opt[Boolean]('f', "ftp").optional().valueName("<true (1) or false (0)>").action((x, c) => c.copy(ftp = x))
+        .text("optional, by default - true - uploads files on ftp")
+      help("help").text("prints this usage text")
+    }
+
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        val partnerConnection: Try[PartnerConnection] = if (System.getProperty("os.name").matches(".*Windows.*")) {
+          //actions in case of Windows
+          //username and password validation
           val encr = new Encryption
-          val (userDecr, passDecr) = encr.decrypt
-          userDecr match {
-            case Some(user) => passDecr match {
-              case Some(pass) =>
-                //connect to SalesForce
-                val connection = new SfConnect
-                val partnerConnection: Try[PartnerConnection] = connection.windowsConnect(user, pass)
-                partnerConnection match {
-                  case Success(v) =>
-                    //query emails and ftp
-                    val query = new SfQuery
-                    val listOfLinksAndFtp: (List[List[String]], Option[String]) = query.prepareResults(partnerConnection, caseNumber)
-
-                    //download attachments locally and upload to ftp, if it exists
-                    val down = new SfDownload
-                    down.download(listOfLinksAndFtp, caseNumber)
-                  case Failure(ex) => println(ex)
-                }
-
-              case None => println("Unable to get password.\nPlease run jar with all 3 parameters.")
+          val (userName, password): (String, String) = if (config.userName.isEmpty || config.passwordToken.isEmpty) {
+            encr.decrypt match {
+              case (Some(user), Some(pass)) => (user, pass)
+              case _ =>
+                System.exit(-1)
+                ("none", "none")
             }
-            case None => println("Please run jar with all 3 parameters.")
+          } else {
+            encr.encrypt(config.userName, config.passwordToken)
+            (config.userName, config.passwordToken)
           }
-        case 3 =>
-          val caseNumber = args(0)
-          if (caseNumber.length != 8) {
-            println("Please enter valid case number.")
-            System.exit(1)
-          }
-          val userName = args(1)
-          val password = args(2)
-
           //connect to SalesForce
           val connection = new SfConnect
-          val partnerConnection: Try[PartnerConnection] = connection.windowsConnect(userName, password)
-          partnerConnection match {
-            case Success(v) =>
-              //encrypt password and write to file
-              val encr = new Encryption
-              encr.encrypt(userName, password)
-
-              //query emails and ftp
-              val query = new SfQuery
-              val listOfLinksAndFtp: (List[List[String]], Option[String]) = query.prepareResults(partnerConnection, caseNumber)
-
-              //download attachments locally and upload to ftp, if it exists
-              val down = new SfDownload
-              down.download(listOfLinksAndFtp, caseNumber)
-            case Failure(ex) => println(ex)
-          }
-        case _ =>
-          println(s"\nPlease run jar with parameters:\n\n" +
-            "- case number\n" +
-            "- username (required for the first run of the program)\n" +
-            "- passwordSecurityToken (no space)(required for the first run of the program)\n\n" +
-            "Example:\n\n" +
-            """PS C:\Users\Administrator> java -jar "C:\temp\download.jar" "12345678" "username@domain.com" "PasswordSecurityToken" """ + "\n")
-      }
-    }
-    else {
-      //actions in case of Linux
-      args.length match {
-        case 1 =>
-          val caseNumber = args(0)
-          if (caseNumber.length != 8) {
-            println("Please enter valid case number.")
-            System.exit(1)
-          }
-
+          connection.windowsConnect(userName, password)
+        }
+        else {
+          //actions in case of Linux
           //connect to SalesForce
           val connection = new SfConnect
-          val partnerConnection: Try[PartnerConnection] = connection.connect
+          connection.connect
+        }
 
-          //query emails and ftp
-          val query = new SfQuery
-          val listOfLinksAndFtp: (List[List[String]], Option[String]) = query.prepareResults(partnerConnection, caseNumber)
+        partnerConnection match {
+          case Success(_) =>
+            //query emails and ftp
+            val query = new SfQuery
+            val listOfLinks: List[List[String]] =
+              query.prepareResults(partnerConnection, config.caseNumber, config.only)
+            val ftpLink: Option[String] = if (config.ftp) query.getFtp(partnerConnection, config.caseNumber) else None
 
-          //download attachments locally and upload to ftp, if it exists
-          val down = new SfDownload
-          down.download(listOfLinksAndFtp, caseNumber)
+            //download attachments locally
+            val down = new SfDownload
+            down.download(listOfLinks, config.caseNumber, config.targetFolder, config.threads, ftpLink)
+          case Failure(ex) => println(ex)
+        }
 
-        case _ => println(s"\nWrong parameters! For Linux please specify case number.\n" +
-          "Example:\n\n" +
-          """root@localhost$ java -jar "C:\temp\download.jar" 12345678 """ + "\n")
-      }
+      case None => // arguments are bad, error message will be displayed
     }
-
   }
 
 }
